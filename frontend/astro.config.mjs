f16101901917faa5eb47bf3480d1e85705755e15
kukur dell <kukurdell@gmail.com>
2026-09-08 22:26:23 +0700

    fix: switch astro to static output for deployment

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'static',
   image: {
     domains: ['localhost', '127.0.0.1'],
   },
