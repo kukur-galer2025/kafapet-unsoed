5ffe190540b594b858b8e58edcbecab302c925de
kukur dell <kukurdell@gmail.com>
2026-09-08 22:29:55 +0700

    fix: revert to SSR mode, run as Node.js server

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,14 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   image: {
     domains: ['localhost', '127.0.0.1'],
   },
   server: {
-    port: 4321
+    port: 4321,
+    host: true,
   }
 });
